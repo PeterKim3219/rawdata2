241748c3175824a23b0663fb10fe0aca010b2c46
PeterKim3219 <peterkim773@gmail.com>
2020-07-31 15:00:24 +0900

    Update homework_week03.py

--- homework_week03.py
+++ homework_week03.py
@@ -16,9 +16,9 @@ trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 # 아래 빈 칸('')을 채워보세요
 try :
     for tr in trs:
-        rank = tr.select_one('#body-content > div.newest-list > div > table > tbody > tr > td.number').text[0:2].strip()
-        title = tr.select_one('#body-content > div.newest-list > div > table > tbody > tr > td.info > a.title.ellipsis').text.strip()
-        artist = tr.select_one('#body-content > div.newest-list > div > table > tbody > tr > td.info > a.artist.ellipsis').text.strip()
+        rank = tr.select_one('td.number').text[0:2].strip()
+        title = tr.select_one('td.info > a.title.ellipsis').text.strip()
+        artist = tr.select_one('td.info > a.artist.ellipsis').text.strip()
 
         doc = {
             'rank':rank,
